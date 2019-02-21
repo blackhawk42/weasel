4c5ff611f5ee8c435e59e0f444a96b3c34e3b58f
Daniel Chávez <danielsaul96@gmail.com>
2019-02-21 17:59:46 -0600

    Changed horrible sort to find the fittest to a linear search

--- functions.go
+++ functions.go
@@ -17,15 +17,15 @@ func GetFitness(rTarget, rSon []rune) (int, error){
 	if len(rTarget) != len(rSon) {
 		return 0, fmt.Errorf("getting fitness: both rune slices must be of the same size");
 	}
-	
+
 	fitness := 0
-	
+
 	for i := range rTarget {
 		if rTarget[i] == rSon[i] {
 			fitness++
 		}
 	}
-	
+
 	return fitness, nil
 }
 
@@ -36,7 +36,7 @@ func GetFitness(rTarget, rSon []rune) (int, error){
 // rune strings to avoid multiple conversions.
 func Mutate(parentRunes []rune, mutationRate float64) []rune {
 	result := make([]rune, len(parentRunes))
-	
+
 	for i := range parentRunes {
 		if rand.Float64() < mutationRate {
 			result[i] = ALPHABET[ rand.Intn(len(ALPHABET)) ]
@@ -44,17 +44,40 @@ func Mutate(parentRunes []rune, mutationRate float64) []rune {
 			result[i] = parentRunes[i]
 		}
 	}
-	
+
 	return result
 }
 
 // Completely random rune slice formed by the offcial alphabet
 func RandomRuneSlice(length int) []rune {
 	result := make([]rune, length)
-	
+
 	for i := range result {
 		result[i] = ALPHABET[ rand.Intn(len(ALPHABET)) ]
 	}
-	
+
 	return result
+}
+
+// FindFittest finds the fittest Offspring in an OffspringSlice. Returns
+// both the index and a pointer to the Offspring itself.
+//
+// It's a linear search, so in case of multiple fittest Offsprings, it will
+// return the first found, but will have searched the entire OffspringSlice to make
+// sure. In case of an Overman, it will return immediately.
+func FindFittest(offs OffspringSlice) (int, *Offspring) {
+	maxFitnessFound := 0
+	fittest := 0
+	for i, currOff := range offs {
+		if currOff.Overman() {
+			return i, currOff
+		}
+
+		if currOff.Fitness > maxFitnessFound {
+			maxFitnessFound = currOff.Fitness
+			fittest = i;
+		}
+	}
+
+	return fittest, offs[fittest];
 }
--- weasel.go
+++ weasel.go
@@ -5,7 +5,7 @@ import (
 	"flag"
 	"os"
 	"path/filepath"
-	"sort"
+	//"sort"
 	"math/rand"
 	"time"
 )
@@ -21,63 +21,64 @@ func main() {
 	var mutationRate = flag.Float64("mutation-rate", DEFAULT_MUTATION_RATE, "mutation `rate`, expressed as a percentage [0.0, 1.0]")
 	var targetPhrase = flag.String("target", DEFAULT_TARGET_FRASE, "target `phrase` aginst which fitness will be measured")
 	var maxGenerations = flag.Int("max-generations", DEFAULT_MAX_GENERATIONS, "maximum number of `generations`, to avoid an endless orgy")
-	
+
 	flag.Usage = func () {
 		fmt.Fprintf(os.Stderr, "use: %s [OPTIONS]\n\n", filepath.Base(os.Args[0]) )
 		fmt.Printf("Slightly shitty version of Richard Dawkins' \"weasel\" algortihm, from \"The Blind Watchmaker\".\n\n")
 		flag.PrintDefaults()
 	}
-	
+
 	flag.Parse()
-	
-	
+
+
 	if *offspringNumber < 1 {
 		fmt.Fprintf(os.Stderr, "please use a positive integer for number of offsprings\n")
 		flag.Usage()
 		os.Exit(2)
 	}
-	
+
 	if *mutationRate < 0.0 || *mutationRate > 1.0 {
 		fmt.Fprintf(os.Stderr, "please use a mutation rate in the range [0.0, 1.0]\n")
 		flag.Usage()
 		os.Exit(2)
 	}
-	
+
 	// Random
-	
+
 	rand.Seed(time.Now().UnixNano())
-	
+
 	// Mian logic
-	
-	
+
+
 	fmt.Printf("Mutation rate: %.2f\n", *mutationRate)
 	fmt.Printf("Number of offsprings per generation: %d\n", *offspringNumber)
 	fmt.Printf("Max number of generations: %d\n", *maxGenerations)
 	fmt.Printf("Target phrase: %s\n", *targetPhrase)
 	fmt.Printf("\n")
-	
-	
+
+
 	offsprings := NewOffspringSlice(*offspringNumber)
-	fittest := len(offsprings) - 1
-	
+	//fittest := len(offsprings) - 1
+	fittest := 0
+
 	var err error
 	offsprings[ fittest ], err = CreateAdam( []rune(*targetPhrase) )
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "creating Adam: %v\n", err)
 		os.Exit(1)
 	}
-	
+
 	fmt.Printf("Adam: %s\n", offsprings[ fittest ].Report())
-	
+
 	if offsprings[ fittest ].Overman() {
 		fmt.Printf("A miracle!!!\n")
 		os.Exit(0)
 	}
-	
-	
+
+
 	for currentGeneration := 1; currentGeneration <= *maxGenerations; currentGeneration++ {
 		parent := offsprings[ fittest ]
-		
+
 		for i := range offsprings {
 			offsprings[i], err = parent.Spawn(*mutationRate)
 			if err != nil {
@@ -85,18 +86,19 @@ func main() {
 				os.Exit(1)
 			}
 		}
-		
-		sort.Sort(offsprings)
-		
+
+		//sort.Sort(offsprings) // Meant as personal practice for the Sort interface
+		fittest, _ = FindFittest(offsprings)
+
 		fmt.Printf("%s\n", offsprings[ fittest ].Report() )
-		
+
 		if offsprings[ fittest ].Overman() {
 			break
 		}
 	}
-	
+
 	fmt.Printf("\n")
-	
+
 	if offsprings[ fittest ].Overman() {
 		fmt.Printf("Overman found!\n")
 	} else {
